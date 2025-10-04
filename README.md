a9cb23e4f7d0752155ba58017c529ab8a408f8df
Isabella <isabella.steyn.student@gmail.com>
2025-10-04 11:37:13 +0200

    Hello World

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # example-repo
+Name: hello world
+Description: inputs name and age. Prints out the name, age, and hello world.
---
+++ hello_world.py
@@ -0,0 +1,11 @@
+# request user for name
+# print out the user name
+# ask user for age
+# print out age
+# print out "Hello, world!" at the end
+
+name = input("Enter name: ")
+print(name)
+age = input("Enter age: ")
+print(age)
+print("Hello, world!")
